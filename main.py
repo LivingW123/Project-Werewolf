b18ac2ea2f6bc7992d4cf00bd7f76ed079a05c1a
LivingW123 <onealwang0202@gmail.com>
2023-07-11 21:56:21 +0800

    geocoder gets location

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World.")
+import geocoder
+import ephem
+
+
+def get_current_location():
+    g = geocoder.ip('me')
+    if g.latlng:
+        return g.latlng
+    else:
+        return None
+
+location = get_current_location()
+if location:
+    ulatitude, ulongitude = location
+    print(f"Latitude: {ulatitude}, Longitude: {ulongitude}")
+else:
+    print("Unable to retrieve current location.")
+
+
+# root.mainloop()
